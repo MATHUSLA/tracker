f13e3bba35f287ac61a59031960514b84a9ec8c6
bhgomes <bhgomes.github@gmail.com>
2018-08-13 12:18:56 -0400

    open up reader internals

--- include/tracker/reader.hh
+++ include/tracker/reader.hh
@@ -20,11 +20,15 @@
 #define TRACKER__READER_HH
 #pragma once
 
+#include <fstream>
+
 #include <tracker/core/type.hh>
 #include <tracker/core/units.hh>
 #include <tracker/analysis/type.hh>
 #include <tracker/analysis/monte_carlo.hh>
 #include <tracker/geometry.hh>
+
+#include <tracker/util/string.hh>
 
 namespace MATHUSLA { namespace TRACKER {
 
@@ -191,6 +195,138 @@ const analysis::mc::full_event_vector_bundle import_full_event_mc_bundle(const s
 } /* namespace root */ /////////////////////////////////////////////////////////////////////////
 
 namespace script { /////////////////////////////////////////////////////////////////////////////
+
+namespace reserved { ///////////////////////////////////////////////////////////////////////////
+//__Reserved Symbols____________________________________________________________________________
+static const char comment_character           = '#';
+static const char space_character             = ' ';
+static const char key_value_separator         = ':';
+static const std::string& continuation_string = "...";
+//----------------------------------------------------------------------------------------------
+} /* namespace reserved */ /////////////////////////////////////////////////////////////////////
+
+//__Parse Line from Tracking Script_____________________________________________________________
+void parse_line(const std::string& line,
+                std::string& key,
+                std::string& value);
+//----------------------------------------------------------------------------------------------
+
+//__Parse Key Value Pair into File Path_________________________________________________________
+void parse_file_path(const std::string& key,
+                     const std::string& value,
+                     std::string& out,
+                     bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Parse Data Key Value Pair___________________________________________________________________
+void parse_data_keys(const std::string& key,
+                     const std::string& value,
+                     std::string& t_key,
+                     std::string& x_key,
+                     std::string& y_key,
+                     std::string& z_key,
+                     bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Parse Boolean Key Value_____________________________________________________________________
+void parse_boolean(const std::string& key,
+                   const std::string& value,
+                   bool& out,
+                   bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Parse Real Key Value________________________________________________________________________
+void parse_real(const std::string& key,
+                const std::string& value,
+                real& out,
+                bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Parse Positive Real Key Value_______________________________________________________________
+void parse_positive_real(const std::string& key,
+                         const std::string& value,
+                         real& out,
+                         bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Parse Real Range Key Value__________________________________________________________________
+void parse_real_range(const std::string& key,
+                      const std::string& value,
+                      real_range& out,
+                      bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Parse Integer Key Value_____________________________________________________________________
+void parse_integer(const std::string& key,
+                   const std::string& value,
+                   integer& out,
+                   bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Parse Size Type Key Value___________________________________________________________________
+void parse_size_type(const std::string& key,
+                     const std::string& value,
+                     std::size_t& out,
+                     bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Parse R4 Key Value__________________________________________________________________________
+void parse_r4_point(const std::string& key,
+                    const std::string& value,
+                    r4_point& out,
+                    bool use_units=true,
+                    bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Parse R3 Coordinate Key Value_______________________________________________________________
+void parse_r3_coordinate(const std::string& key,
+                         const std::string& value,
+                         Coordinate& coordinate,
+                         bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Parse R4 Coordinate Key Value_______________________________________________________________
+void parse_r4_coordinate(const std::string& key,
+                         const std::string& value,
+                         Coordinate& coordinate,
+                         bool exit_on_error=true);
+//----------------------------------------------------------------------------------------------
+
+//__Read and Parse Lines from Tracking Script___________________________________________________
+template<class TernaryFunction>
+void parse_lines(std::ifstream& file,
+                 TernaryFunction f) {
+  std::string line;
+  while (std::getline(file, line)) {
+    if (line.empty()) continue;
+    std::string key, value;
+    f(line, key, value);
+  }
+}
+template<class TernaryFunction>
+void parse_lines(const std::string& path,
+                 TernaryFunction f) {
+  std::ifstream file{path};
+  parse_lines(file, f);
+}
+//----------------------------------------------------------------------------------------------
+
+//__Read Lines from Tracking Script_____________________________________________________________
+template<class BinaryFunction>
+void read_lines(std::ifstream& file,
+                BinaryFunction f) {
+  parse_lines(file, [&](const auto& line, auto& key, auto& value) {
+    parse_line(line, key, value);
+    f(key, value);
+  });
+}
+template<class BinaryFunction>
+void read_lines(const std::string& path,
+                BinaryFunction f) {
+  std::ifstream file{path};
+  read_lines(file, f);
+}
+//----------------------------------------------------------------------------------------------
 
 //__Tracking Script Options Parser______________________________________________________________
 const tracking_options read(const std::string& path);
